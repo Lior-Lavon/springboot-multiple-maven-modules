18af7cecd2c71b679aef615c0d0cea3f4d95e8f8
Lior Lavon <>
2020-05-07 12:00:54 +0200

    addign GitLab CI - test

---
+++ web/src/test/java/rc/web/HotelControllerTest.java
@@ -0,0 +1,19 @@
+package rc.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelControllerTest {
+
+    HotelController hotelController;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getHotels() {
+    }
+}
